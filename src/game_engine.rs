5f3471a954b2cc2f00790851d9053dacc7aa43a5
camagarvey <camagarvey@gmail.com>
2024-03-25 21:35:43 +0100

    fully functioning game

--- src/game_engine.rs
+++ src/game_engine.rs
@@ -57,18 +57,40 @@ impl Board {
         let target: char = self.get_move();
         if self.cheack_legal_move(target) {
             self.make_move(target, self.turn);
+        } else {
+            self.player_move();
         }
     }
 
+    pub fn check_winner(&self) -> bool {
+        let winning_combinations: [[usize; 3]; 8] = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Vertical
+            [0, 4, 8], [2, 4, 6],             // Diagonal
+        ];
+
+        for &combo in winning_combinations.iter() {
+            let symbols: Vec<char> = combo.iter().map(|&i| self.board[i]).collect();
+            if symbols.iter().all(|&symbol| symbol == self.turn) {
+                println!("{} Wins!", self.turn);
+                return true;
+            }
+        }
+
+        false
+    }
+
+    pub fn check_tie(&self) -> bool {
+        println!("It's a tie!");
+        self.board.iter().all(|&symbol| symbol == 'x' || symbol == 'o')
+     
+    }
 }   
 
 pub struct Board {
     pub board: [char; 9],
     pub turn: char,
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,19 @@ pub use crate::game_engine::game_engine as game;
 
 pub fn run() {
     let mut board=game::Board::new(); 
-    board.print_board();
-    board.player_move();
-    board.print_board();
+    loop {
+        board.print_board();
+        board.player_move();
+        if board.check_winner() || board.check_tie(){
+            break
+        }
+        if board.turn == 'x' {
+            board.turn = 'o';
+        } else {
+            board.turn = 'x'
+        }
+    }
+   
+
 }
 
